659b7225d28ee44b6d40a941e3b46b136d760509
Jeff Allen <jamaps@users.noreply.github.com>
2022-03-03 08:53:51 -0500

    Update map_all.js

--- map_all.js
+++ map_all.js
@@ -10,7 +10,7 @@ var cstage = 1
 var remap = 0
 var score = 0
 var w = ""
-mapboxgl.accessToken = 'meow';
+mapboxgl.accessToken = 'pk.eyJ1IjoiamVmZmFsbGVuIiwiYSI6ImNrbGgyMXFjaDB6aXoyd29pNmF4NTRyMWwifQ.vwE_3WfPjbTW8cMoDKbq6A';
 
 
 
